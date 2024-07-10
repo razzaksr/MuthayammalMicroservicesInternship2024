3794b25b09f4780c273c17e90add546915ab5c10
razzaksr <razzaksr@gmail.com>
2024-07-10 09:59:32 +0530

    List CRUD done

--- spring-poc-crud/src/main/java/cse/poc/spring_poc_crud/BasicRest.java
+++ spring-poc-crud/src/main/java/cse/poc/spring_poc_crud/BasicRest.java
@@ -1,32 +1,53 @@
 package cse.poc.spring_poc_crud;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // back end router: accept the request and process
 @RestController
 public class BasicRest {
-    private String[] industryVerticals={
-            "Banking","Logistics","Health",
-            "Automation","Government","Transport"
-    };
+
+    private List<String> industryVerticals=new ArrayList<>();
+
+    @PostConstruct
+    // invoke as soon as controller initiated by executing app
+    public void initialize(){
+        industryVerticals.add("Banking");
+        industryVerticals.add("Health");
+        industryVerticals.add("Logistics");
+        industryVerticals.add("Travels");
+        industryVerticals.add("Insurance");
+    }
+
+    @PutMapping("/change/{index}")
+    public String modify(@PathVariable("index") int index,@RequestBody String alternate){
+        industryVerticals.set(index,alternate);
+        return alternate+" has replaced at "+index;
+    }
+
+    @PostMapping("/client")
+    public String agree(@RequestBody String newIndustry){
+        industryVerticals.add(newIndustry);
+        return newIndustry+" has considered as new client";
+    }
 
     @DeleteMapping("/remove/{index}")
     public String invalidate(@PathVariable("index") int index){
-        String response = industryVerticals[index]+" has deleted";
-        industryVerticals[index]=null;
+        String response = industryVerticals.get(index)+" has deleted";
+        industryVerticals.remove(index);
         return response;
     }
 
     @GetMapping("/particular/{index}")
     public String getOneIndustry(@PathVariable("index") int index){
-        return industryVerticals[index];
+        return industryVerticals.get(index);
     }
 
     @GetMapping("/industries")
-    public String[] getIndustries(){
+    public List<String> getIndustries(){
         return industryVerticals;
     }
 }
