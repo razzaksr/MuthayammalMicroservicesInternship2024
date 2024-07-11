5f26daa78c315f0290af40bdfc916fff37d9cee7
razzaksr <razzaksr@gmail.com>
2024-07-11 13:02:14 +0530

    JPA Connectvity done

--- spring-poc-crud/src/main/java/cse/poc/spring_poc_crud/PatientRemote.java
+++ spring-poc-crud/src/main/java/cse/poc/spring_poc_crud/PatientRemote.java
@@ -1,2 +1,18 @@
-package cse.poc.spring_poc_crud;public interface PatientRemote {
+package cse.poc.spring_poc_crud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+save>>insert/update object into relational rows
+findAll
+findById
+findByPropertyName
+findAllByPropertyName
+deleteById
+ */
+
+@Repository
+public interface PatientRemote extends JpaRepository<Patient,Integer> {
+
 }
--- spring-poc-crud/src/main/java/cse/poc/spring_poc_crud/PatientService.java
+++ spring-poc-crud/src/main/java/cse/poc/spring_poc_crud/PatientService.java
@@ -1,2 +1,24 @@
-package cse.poc.spring_poc_crud;public class PatientService {
+package cse.poc.spring_poc_crud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    @Autowired
+    PatientRemote remote;
+
+    public void implementDelete(int id){
+        remote.deleteById(id);
+    }
+
+    public List<Patient> implementFindAll(){
+        return remote.findAll();
+    }
+
+    public Patient implementSave(Patient patient){
+        return remote.save(patient);
+    }
 }
