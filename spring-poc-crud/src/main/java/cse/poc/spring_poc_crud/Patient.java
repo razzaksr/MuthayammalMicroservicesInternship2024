01717d0b26f7eff5c24c5992fee9abc23c246df3
razzaksr <razzaksr@gmail.com>
2024-07-11 10:13:43 +0530

    Spring boot JPA entity created

--- spring-poc-crud/src/main/java/cse/poc/spring_poc_crud/Patient.java
+++ spring-poc-crud/src/main/java/cse/poc/spring_poc_crud/Patient.java
@@ -1,5 +1,9 @@
 package cse.poc.spring_poc_crud;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +14,13 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+// table creation
+@Entity
 public class Patient {
+    // primary key
+    @Id
+    // auto increment
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int patientId;
     private String patientName;
     private int age;
